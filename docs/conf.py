708b8e28f3fa41c5374b47f28e9ebeea121d4a2f
Alison Peisker <apeisker@fnal.gov>
2025-09-09 13:27:05 -0500

    moving docs to github pages

--- docs/conf.py
+++ docs/conf.py
@@ -52,9 +52,19 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-html_theme = 'nature'
+#html_theme = 'alabaster'
+#html_theme = 'nature'
+html_theme = 'sphinx_book_theme'
 
+copybutton_prompt_text = r"\$ "
+copybutton_prompt_is_regexp = True
+
+html_theme_options = {
+    "repository_url": "https://github.com/fermitools/data_dispatcher",
+    "use_repository_button": True,
+    "navigation_depth": 4,  # how many levels of nested headers show in sidebar
+    "collapse_navigation": False
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
