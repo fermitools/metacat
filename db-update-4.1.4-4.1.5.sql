129bc74e18da96b4893d403a3be812dcb4c3fe9f
Alison Peisker <apeisker@fnal.gov>
2026-09-17 14:09:53 -0500

    move file

--- db-update-4.1.4-4.1.5.sql
+++
@@ -1,9 +0,0 @@
-
--- get in the metacat schema.
-
-set search_path = metacat;
-
--- in 4.1.5 we will need the "required" column in 
--- the paramater_categories table.
-
-alter table parameter_categories add column required boolean default 'false';
